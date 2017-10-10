9ee5a4f0f6a83b66eb676415e833ca492fd5fb93
venkatyanamandala <32670972+venkatyanamandala@users.noreply.github.com>
2017-10-10 13:03:42 +0100

    Create jsonapi.php

---
+++ jsonapi.php
@@ -0,0 +1,6 @@
+<?php
+$items = array();
+for($i = 0; $i <10; $i++) { $items [$i] = "This is message $i" }
+header('Connect-Type: application/json');
+$jsonOutput = json_encodes($items);
+echo $jsonOutput;
